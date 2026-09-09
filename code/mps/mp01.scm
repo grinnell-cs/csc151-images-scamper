386c57def4145978d4755e60b46c2df8e4f87e29
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2026-09-08 20:15:46 -0500

    Add code for MP01.

---
+++ code/mps/mp01.scm
@@ -0,0 +1,52 @@
+;; mp01.scm
+;;   An exploration of images, decomposition, composition, and fnctions.
+;;
+;; CSC-151-NN Fall 2026
+;;
+;; Author: YOUR NAME HERE
+;;
+;; Date: YYYY-MM-DD
+;;
+;; Acknowledgements
+;; * Starter code was provided with this file.
+
+(import image)
+
+;; +-----------------------------+------------------------------------
+;; | Part 1: A rainbow spaceship |
+;; +-----------------------------+
+
+;;; rainbow-spaceship : image?
+;;; A rainbow spaceship that follows the guidelines in the assignment.
+
+
+;; +-------------------+----------------------------------------------
+;; | Part 2: Freestyle |
+;; +-------------------+
+
+;;; my-image : image?
+;;; INSERT A DESCRIPTION
+
+
+;; +------------------------------------+-----------------------------
+;; | Part 3: Your own library functions |
+;; +------------------------------------+
+
+;; +---------------------------------+--------------------------------
+;; | Part 4: Generalizing your image |
+;; +---------------------------------+
+
+;;; (generate-my-image ...) -> image?
+;;;    DOCUMENTATION FOR PARAMETERS
+;;; DESCRIPTION OF WHAT IT DOES
+
+
+;;; my-image-alt : image?
+;;; Something that looks surprisingly like `my-image`. However, this time
+;;; it was created with `generate-my-image`.
+
+
+;;; my-other-image : image?
+;;; A variant of `my-image` (e.g., different color, size).
+
+
